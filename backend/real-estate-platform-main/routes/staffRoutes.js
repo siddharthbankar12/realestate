28f5997480e304059d9f3782f53ffcd10710c32c
thala192 <csrh8278@gmail.com>
2025-06-17 14:49:13 +0530

    Update staffRoutes.js

--- backend/real-estate-platform-main/routes/staffRoutes.js
+++ backend/real-estate-platform-main/routes/staffRoutes.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getPerformanceTiers } = require('../controllers/performanceTierController');
-const { getPerformanceStats } = require('../controllers/performanceStatsController');
 
-router.get('/performance-tiers', getPerformanceTiers);
-router.get('/performance-stats', getPerformanceStats);
-
-module.exports = router;
